acb56f143f25b288cda6b9304f4dc4c220b09c60
Nellybeauty24 <anayonnenna9@gmail.com>
2025-07-07 22:44:21 +0100

    updates

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    burger.classList.toggle("toggle"); 
+  });
+});
